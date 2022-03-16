588440e132771cdf3240970fc94a994b315e4aa8
Francisco Javier Cozar Moreno <pakeraskeras@gmail.com>
2022-03-16 20:18:16 +0100

    Ejercicio4

---
+++ ejercicio4b.py
@@ -0,0 +1,6 @@
+for numero in range(0,14):
+    if numero % 2 == 0:
+        continue
+    else:
+        print(numero)
+
---
+++ ejercicio4c.py
@@ -0,0 +1,5 @@
+lista = []
+for numero in range(1,101):
+    lista.append(numero)
+reverso = sorted(lista, reverse=True)
+print(reverso)
